0e5b2bb6801d9250ba90edd3e66adc469d1c2cf0
Alexander Beley <beleysasha@gmail.com>
2025-11-13 16:44:14 +1200

    database added

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,18 @@
-from flask import (Flask, render_template)
+import os
+from datetime import date
 
+from flask import (Flask, render_template, url_for, request, flash, redirect, abort)
+import psycopg2
+from dotenv import load_dotenv
+
+from url_repository import URLRepository
+from validators.url import url as check_url
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
+repo = URLRepository(conn)
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
@@ -7,7 +20,53 @@ app.secret_key = 'secret_key'
 @app.route("/")
 def index():
     context = 'Context'
+    errors = {}
     return render_template(
         'index.html',
-        context=context
+        context=context, errors=errors
+    )
+
+
+@app.route('/urls')
+def urls_list():
+    urls = repo.get_content()
+    errors = {}
+    return render_template(
+        'urls/index.html',
+        urls=urls,
+        errors=errors
+    )
+
+
+
+@app.route('/urls', methods=['POST'])
+def urls_post():
+    url_data = request.form.to_dict()
+    errors = {}
+    is_valid_url = check_url(url_data['url'])
+    if not is_valid_url:
+        errors['url'] = 'Некорректный URL'
+    if errors:
+        return render_template(
+            'index.html',
+            url=url_data,
+            errors=errors
+        ), 422
+    created_at = date.today()
+    url = {'name': url_data['url'], 'created_at': created_at}
+    id = repo.save(url)
+    flash('Site was added successfully', 'success')
+    return redirect(url_for('urls_show', id=id, errors=errors))
+
+
+@app.route('/urls/<int:id>')
+def urls_show(id):
+    errors = {}
+    url = repo.find(id)
+    if not url:
+        abort(404)
+    return render_template(
+        'urls/show.html',
+        url=url,
+        errors=errors
     )
---
+++ url_repository.py
@@ -0,0 +1,56 @@
+import psycopg2
+from psycopg2.extras import DictCursor
+
+
+class URLRepository:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def get_content(self):
+        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("SELECT * FROM urls")
+            return [dict(row) for row in cur]
+
+    def find(self, id):
+        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
+            row = cur.fetchone()
+            return dict(row) if row else None
+
+    def get_by_term(self, search_term=""):
+        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute(
+                """
+                    SELECT * FROM urls
+                    WHERE name ILIKE %s
+                """,
+                (f"%{search_term}%", f"%{search_term}%"),
+            )
+            return cur.fetchall()
+
+    def save(self, url):
+        if "id" in url and url["id"]:
+            self._update(url)
+            
+        else:
+            self._create(url)
+        return url["id"]
+
+
+    def _update(self, url):
+        with self.conn.cursor() as cur:
+            cur.execute(
+                "UPDATE urls SET name = %s",
+                (url["name"], url["created_at"], url["id"]),
+            )
+        self.conn.commit()
+
+    def _create(self, url):
+        with self.conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id",
+                (url["name"], url["created_at"]),
+            )
+            id = cur.fetchone()[0]
+            url["id"] = id
+        self.conn.commit()
